52bab6c6d717c1a4b419bcbbc1bf0763a6e2c50c
unlimited <osx@pop-os.localdomain>
2023-07-11 15:31:33 -0700

    str

---
+++ screens/FinishScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FinishScreen = () => {
+  return (
+	<View>
+	  <Text>FinishScreen</Text>
+	</View>
+  )
+}
+
+export default FinishScreen
---
+++ screens/StartingScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const StartingScreen = () => {
+  return (
+	<View>
+	  <Text>StartingScreen</Text>
+	</View>
+  )
+}
+
+export default StartingScreen
